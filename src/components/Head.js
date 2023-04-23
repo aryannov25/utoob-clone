a7fa1b4c9fa1eb387ed53dc4f6bfadc662899cc5
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-23 13:23:39 +0530

    update

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,22 +1,21 @@
 import React from "react";
 import Logo from "../img/logo.png";
+import HamMenu from "../img/menu.svg";
 
 function Head() {
   return (
-    <div>
-      <div>
-        <img
-          alt="menu"
-          src="https://www.svgrepo.com/show/312300/hamburger-menu.svg"
-        />
-        <img alt="logo" src={Logo} />
+    <div className="grid grid-flow-col p-4 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img className="h-8" alt="menu" src={HamMenu} />
+        <img className="h-8 mx-2" alt="logo" src={Logo} />
       </div>
-      <div>
-        <input type="text" />
-        <button>Search</button>
+      <div className="col-span-10 text-center">
+        <input className="w-1/2 p-1 border border-gray-400" type="text" />
+        <button className="p-1  border border-gray-400">Search</button>
       </div>
-      <div>
+      <div className="col-span-1">
         <img
+          className="h-8"
           alt="user"
           src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
         />
