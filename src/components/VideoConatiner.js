94ca4bc82157e5a1eae869aa7d8c1e1ebb130a01
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-24 12:51:09 +0530

    update

--- src/components/VideoConatiner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const VideoConatiner = () => {
-  return (
-    <div>VideoConatiner</div>
-  )
-}
-
-export default VideoConatiner
--- src/utils/store.js
+++ src/utils/store.js
@@ -5,3 +5,5 @@ const store = configureStore({
 
 
 })
+
+export default store;
