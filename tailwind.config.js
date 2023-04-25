668dce92316f67f99932d8d65b9cf10fbb3f4c05
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-25 13:05:47 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.js"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1536px',
+      // => @media (min-width: 1024px) { ... }
+    },
   },
   plugins: [],
 }
