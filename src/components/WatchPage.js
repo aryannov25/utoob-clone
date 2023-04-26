e9c929fc7720925e74e644b9b5fc3969b800c0c3
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-27 00:01:28 +0530

    update

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    
-  }, []);
+    dispatch(closeMenu());
+  });
 
   return <div>WatchPage</div>;
 };
