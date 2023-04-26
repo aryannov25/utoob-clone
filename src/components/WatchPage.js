ee18937d46eb3a146cea83d5e4a491c01c1cc8a4
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-27 00:14:50 +0530

    update

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
 
   useEffect(() => {
     dispatch(closeMenu());
   });
 
-  return <div>WatchPage</div>;
+  return (
+    <div className="p-5">
+      <iframe
+        width="1200"
+        height="600"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
