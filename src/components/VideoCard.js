60760e041cf1a327f2b0bf707c32f7b2816e7239
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-27 19:14:38 +0530

    update

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const VideoCard = ({ info }) => {
-    console.log(info);
+  console.log(info);
 
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
 
   return (
-    <div className="p-2 m-2 w-72 shadow-lg rounded-lg transition duration-500 ease-in-out hover:scale-105 hover:shadow-slate-400">
+    <div className="p-2 m-2 w-72 h-80 shadow-lg rounded-lg transition duration-500 ease-in-out hover:scale-105 hover:shadow-slate-400">
       <img className="rounded-lg" src={thumbnails.medium.url} alt="thumbnail" />
       <ul>
         <li className="font-bold">{title}</li>
