efa8ec7f240ebdfca2f32b520d6d4f138315f7a8
Aryan Chaudhary <aryannov25@gmail.com>
2023-04-30 11:07:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import './App.css';
-import Head from './components/Head';
-import Body from './components/Body';
-import store from './utils/store';
-import { Provider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import MainContainer from './components/MainContainer';
-import WatchPage from './components/WatchPage';
+import "./App.css";
+import Head from "./components/Head";
+import Body from "./components/Body";
+import store from "./utils/store";
+import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
 
-const appRouter = createBrowserRouter([{
- path:"/",
- element:<Body/>,
- children:[
+const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<MainContainer/>,
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
   },
-  {
-    path:"watch",
-    element:<WatchPage/>,
-  }
- ]
-}])
+]);
 function App() {
   return (
     <Provider store={store}>
-    <div className="App">
-    <Head />
-    <RouterProvider router={appRouter}></RouterProvider>
-    </div>
+      <div className="App">
+        <Head />
+        <RouterProvider router={appRouter}></RouterProvider>
+      </div>
     </Provider>
   );
 }
