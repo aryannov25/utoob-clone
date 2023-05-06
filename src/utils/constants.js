1304c5052ba7988e211b766ff6f64ede80f9853e
Aryan Chaudhary <aryannov25@gmail.com>
2023-05-07 00:44:54 +0530

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@
 export const YOUTUBE_API_KEY =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   process.env.REACT_APP_GOOGLE_API_KEY;
-  // GOOGLE_API_KEY;
+// GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
   "https://clients1.google.com/complete/search?client=firefox&ds=yt&q=";
