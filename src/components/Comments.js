7277be18a2036b1b4b159a7f3f01e1205a484d89
Aryan Chaudhary <aryannov25@gmail.com>
2023-05-09 11:38:39 +0530

    update

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Comments = () => {
-  return <div>Comments</div>;
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nostrum
+      laboriosam, molestiae recusandae amet repudiandae error consequuntur
+      praesentium officiis quas sapiente iste excepturi tempora cumque eligendi
+      nobis? Quibusdam, ipsa nihil?
+    </div>
+  );
 };
 export default Comments;
