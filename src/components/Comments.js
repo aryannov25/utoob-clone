6998113ad7105fe34d3f54b6e6ad33e50efcb337
Aryan Chaudhary <aryannov25@gmail.com>
2023-05-15 00:59:05 +0530

    update

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Comments = ({ name, text, imglink }) => {
-    // console.log(imglink)
+  // console.log(imglink)
   return (
     <div className="m-5 p-2">
       <div className="flex">
