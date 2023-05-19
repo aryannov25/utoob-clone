5ad0f4e50df7f6854275467fed2cb0f0fa941252
Aryan Chaudhary <aryannov25@gmail.com>
2023-05-20 00:01:58 +0530

    update

--- src/components/VideoSuggestionsCard.js
+++ src/components/VideoSuggestionsCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { prettifyNumber } from "./../utils/number";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const VideoSuggestionsCard = ({ info }) => {
   //  console.log(info);
@@ -11,20 +11,20 @@ const VideoSuggestionsCard = ({ info }) => {
 
   return (
     <a href={"/watch?v=" + info.id.videoId} key={info.id.videoId}>
-    <div className="flex flex-row rounded-lg mt-2 ">
-      <img
-        className="rounded-lg w-[180px] "
-        alt="thumbnail"
-        src={thumbnails.medium.url}
-      />
-      <ul>
-        <li className="p-1 text-sm font-bold">{title}</li>
-        <li className="px-1 text-xs text-stone-500">{channelTitle}</li>
-        <li className="px-1 text-xs text-stone-500">
-          {/* {prettifyNumber(views)} views */}
-        </li>
-      </ul>
-    </div>
+      <div className="flex flex-row rounded-lg mt-2 ">
+        <img
+          className="rounded-lg w-[180px] "
+          alt="thumbnail"
+          src={thumbnails.medium.url}
+        />
+        <ul>
+          <li className="p-1 text-sm font-bold">{title}</li>
+          <li className="px-1 text-xs text-stone-500">{channelTitle}</li>
+          <li className="px-1 text-xs text-stone-500">
+            {/* {prettifyNumber(views)} views */}
+          </li>
+        </ul>
+      </div>
     </a>
   );
 };
