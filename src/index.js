360954558ea726ae6b1be67d68c29cb55c3e4412
Aryan Chaudhary <aryannov25@gmail.com>
2023-05-20 11:47:17 +0530

    update

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <App />
 );
 
 //React.StrictMode causes double console logs
