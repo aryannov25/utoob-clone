240ca138af5a404bcd1c8b9c425f8eaa461fbf38
Aryan Chaudhary <aryannov25@gmail.com>
2023-05-24 11:28:02 +0530

    update

--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { OFFSET_LIVE_CHAT } from "./constants";
 
 const chatSlice = createSlice({
   name: "chat",
@@ -7,6 +8,7 @@ const chatSlice = createSlice({
   },
   reducers: {
     addMessage: (state, action) => {
+      state.messages.splice(OFFSET_LIVE_CHAT, 1);
       state.messages.unshift(action.payload);
     },
   },
