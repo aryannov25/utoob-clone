d547ddb1573fe468a726694a81b2f1bf331ea23a
Aryan Chaudhary <aryannov25@gmail.com>
2023-06-17 00:00:46 +0530

    update

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const ButtonList = () => {
   const slideLeft = () => {
@@ -29,202 +30,202 @@ const ButtonList = () => {
             All
           </button>
         </div>
-        <a href={"/results?search_query=Games"}>
+        <Link to={"/results?search_query=Games"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Games
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Music"}>
+        </Link>
+        <Link to={"/results?search_query=Music"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Music
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Live"}>
+        </Link>
+        <Link to={"/results?search_query=Live"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Live
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Comedy"}>
+        </Link>
+        <Link to={"/results?search_query=Comedy"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Comedy
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Movies"}>
+        </Link>
+        <Link to={"/results?search_query=Movies"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Movies
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Gadgets"}>
+        </Link>
+        <Link to={"/results?search_query=Gadgets"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Gadgets
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=News"}>
+        </Link>
+        <Link to={"/results?search_query=News"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               News
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Musicals"}>
+        </Link>
+        <Link to={"/results?search_query=Musicals"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Musicals
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Trending"}>
+        </Link>
+        <Link to={"/results?search_query=Trending"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Trending
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Raftaar"}>
+        </Link>
+        <Link to={"/results?search_query=Raftaar"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Raftaar
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Drama"}>
+        </Link>
+        <Link to={"/results?search_query=Drama"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Drama
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Yo+Yo+Honey+Singh"}>
+        </Link>
+        <Link to={"/results?search_query=Yo+Yo+Honey+Singh"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Yo Yo Honey Singh
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Theater"}>
+        </Link>
+        <Link to={"/results?search_query=Theater"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Theater
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Cinema"}>
+        </Link>
+        <Link to={"/results?search_query=Cinema"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Cinema
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Reality+Shows"}>
+        </Link>
+        <Link to={"/results?search_query=Reality+Shows"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Reality Shows
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Accounting"}>
+        </Link>
+        <Link to={"/results?search_query=Accounting"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Accounting
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Motivation"}>
+        </Link>
+        <Link to={"/results?search_query=Motivation"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Motivation
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Hip-Hop"}>
+        </Link>
+        <Link to={"/results?search_query=Hip-Hop"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Hip-Hop
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Laptops"}>
+        </Link>
+        <Link to={"/results?search_query=Laptops"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Laptops
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Technology"}>
+        </Link>
+        <Link to={"/results?search_query=Technology"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Technology
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=New+to+you"}>
+        </Link>
+        <Link to={"/results?search_query=New+to+you"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               New to you
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Tailwindcss"}>
+        </Link>
+        <Link to={"/results?search_query=Tailwindcss"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Tailwindcss
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Recently+uploaded"}>
+        </Link>
+        <Link to={"/results?search_query=Recently+uploaded"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Recently uploaded
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Leatures"}>
+        </Link>
+        <Link to={"/results?search_query=Leatures"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Leatures
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Random+Things"}>
+        </Link>
+        <Link to={"/results?search_query=Random+Things"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Random Things
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Javascript"}>
+        </Link>
+        <Link to={"/results?search_query=Javascript"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Javascript
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Bella"}>
+        </Link>
+        <Link to={"/results?search_query=Bella"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Bella
             </button>
           </div>
-        </a>
-        <a href={"/results?search_query=Top+50"}>
+        </Link>
+        <Link to={"/results?search_query=Top+50"}>
           <div>
             <button className="bg-slate-100 dark:bg-zinc-500 rounded-full shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
               Top 50
             </button>
           </div>
-        </a>
+        </Link>
       </div>
       <MdChevronRight
         className="hover:scale-150 my-auto opacity-50 "
