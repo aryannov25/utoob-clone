eb74a2965f26a63725c256c7bc8c42a62ed1718e
Aryan Chaudhary <aryannov25@gmail.com>
2023-06-20 01:18:07 +0530

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,15 +5,23 @@ import { addToCache } from "../utils/CacheSlice";
 import ResultsSuggestionContainer from "./ResultsSuggestionContainer";
 import { showSuggestionsContainer } from "../utils/showSearchSuggestionsSlice";
 import { Link } from "react-router-dom";
+import ClickAwayListener from "react-click-away-listener";
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState();
   const dispatch = useDispatch();
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const cache = useSelector((store) => store.searchSuggestionCache.cache);
   const showSuggestions = useSelector(
     (store) => store.showSearchSuggestions.show
   );
+
+  const handleClickAway = () => {
+    // alert("Maybe close the popup");
+    setMenuOpen(false);
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -60,27 +68,30 @@ const SearchBar = () => {
           e.preventDefault();
         }}
       >
-        <div className="flex h-10 mt-2 pt-[2px] pr-20 ">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="w-[550px] border rounded-s-full px-7 shadow-lg dark:bg-zinc-800"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onFocus={() => dispatch(showSuggestionsContainer(true))}
-          />
-
-          <Link to={`/results?search_query=${searchQuery}`}>
-            <img
-              className="h-[39px] py-2 px-5 border rounded-e-full hover:bg-gray-200 shadow-lg bg-gray-100 dark:bg-zinc-700"
-              alt="search"
-              src="https://cdn-icons-png.flaticon.com/512/3917/3917132.png"
+        <ClickAwayListener onClickAway={handleClickAway}>
+          <div className="flex h-10 mt-2 pt-[2px] pr-20 ">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-[550px] border rounded-s-full px-7 shadow-lg dark:bg-zinc-800"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              // onFocus={() => dispatch(showSuggestionsContainer(true))}
+              // onBlur={() => showSuggestionsContainer(false)}
+              onFocus={setMenuOpen}
             />
-          </Link>
-        </div>
-        {showSuggestions && suggestions.length > 0 && (
-          <div className="absolute bg-white w-[550px] border rounded-lg shadow-lg  font-semibold mx-1 my-[2px]">
-            {" "}
+
+            <Link to={`/results?search_query=${searchQuery}`}>
+              <img
+                className="h-[39px] py-2 px-5 border rounded-e-full hover:bg-gray-200 shadow-lg bg-gray-100 dark:bg-zinc-700"
+                alt="search"
+                src="https://cdn-icons-png.flaticon.com/512/3917/3917132.png"
+              />
+            </Link>
+          </div>
+        </ClickAwayListener>
+        {menuOpen &&  (
+          <div className="z-[9] absolute bg-white w-[550px] border rounded-lg shadow-lg  font-semibold mx-1 my-[2px]">
             {suggestions.map((suggestion) => {
               return (
                 <ResultsSuggestionContainer
