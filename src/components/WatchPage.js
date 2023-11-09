9237fe05b6460f34cd479a42e207cadaec2254fa
Aryan Chaudhary <aryannov25@gmail.com>
2023-11-09 23:57:22 +0530

    .

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -62,7 +62,6 @@ const WatchPage = () => {
               height="600"
               src={"https://www.youtube.com/embed/" + searchParams.get("v")}
               title="YouTube video player"
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             ></iframe>
