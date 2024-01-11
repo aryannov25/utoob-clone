d8fe740d1523f96d582479a3404ba24860d807f0
Aryan Chaudhary <aryannov25@gmail.com>
2024-01-11 20:04:26 +0530

    fixed

--- src/components/ResultsSuggestionContainer.js
+++ src/components/ResultsSuggestionContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const ResultsSuggestionContainer = ({ suggestion }) => {
+const ResultsSuggestionContainer = ({ suggestion, handleClickAway }) => {
   if (!suggestion) return null;
   // console.log(suggestion)
   return (
-    <a href={`/results?search_query=${suggestion}`}>
+    <a
+      href={`/results?search_query=${suggestion}`}
+      onClick={() => handleClickAway()}
+    >
       <div className="dark:bg-zinc-900">
         <p
           className="p-1 px-2 hover:bg-gray-700 
